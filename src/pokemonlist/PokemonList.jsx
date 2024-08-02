d68ea12069ab56b36f51956a32cdff652228ea28
vikas-11-dixena <vikasdixena79@gmail.com>
2024-08-02 13:30:51 +0530

    write some logic in pokemonlist.jsx file to use for data fetch which is required

--- src/pokemonlist/PokemonList.jsx
+++ src/pokemonlist/PokemonList.jsx
@@ -1,19 +1,43 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import "./PokemonList.css"
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./PokemonList.css";
 const PokemonList = () => {
-    async function downloadPokemon() {
-        let response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
-        console.log(response);
-    }
-    useEffect(() => {
-        downloadPokemon();
-    },[])
-  return (
-    <div className='pokemonList-wrapper'>
-      Pokemon List
-    </div>
-  )
-}
+  const [pokemonList, setPokemonList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default PokemonList
+  async function downloadPokemon() {
+    let response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
+    console.log(response);
+    const pokemonResults = response.data.results;
+    console.log(pokemonResults);
+    const pokemonResultPromise = pokemonResults.map((pokemon) =>
+      axios.get(pokemon.url)
+    );
+    console.log(pokemonResultPromise);
+    const pokemonData = await axios.all(pokemonResultPromise);
+    console.log(pokemonData);
+    const result = pokemonData.map((pokeData) => {
+      const pokemon = pokeData.data;
+      console.log(pokemon);
+      return {
+        name: pokemon.name,
+        image: pokemon.sprites.other.dream_world.front_default,
+        types: pokemon.types,
+      };
+    });
+    console.log(result);
+    setPokemonList(result);
+    setIsLoading(false);
+  }
+  useEffect(() => {
+    downloadPokemon();
+  }, []);
+  return (
+    <div className="pokemonList-wrapper">
+      <div>Pokemon List</div>
+      {isLoading ? "Loading...." : "Data download"}
+    </div>
+  );
+};
+
+export default PokemonList;
